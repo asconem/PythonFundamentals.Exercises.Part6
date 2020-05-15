196a974a8d4a79701d8c60cd870cad1c23943e39
asconem <asconem@gmail.com>
2020-05-14 23:11:53 -0400

    Celsius to fahrenheit function working

--- temperature_utils.py
+++ temperature_utils.py
@@ -8,7 +8,7 @@ def convert_to_celsius(fahrenheit_temp: float) -> float:
     :param fahrenheit_temp: A float representing a temperature in fahrenheit
     :return: A float representing the corresponding value of the fahrenheit_temp parameter in celsius
     """
-    celsius = round(((fahrenheit_temp - 32) * 5 / 9), 2)
+    celsius = round(((fahrenheit_temp - 32) * 5/9), 2)
     return celsius
 
 
@@ -19,7 +19,8 @@ def convert_to_fahrenheit(celsius_temp: float) -> int:
     :param celsius_temp: A float representing a temperature in celsius
     :return:  A float representing the corresponding value of the celsius_temp parameter in fahrenheit
     """
-    pass  # remove pass statement and implement me
+    fahrenheit = round(((celsius_temp * (9/5)) + 32), 2)
+    return fahrenheit
 
 
 def temperature_tuple(temperatures: Iterable, input_unit_of_measurement: str) -> Tuple[Tuple[float, float]]:
